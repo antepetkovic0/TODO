fb97f3f6fddcb980da716d1de4e654416c4e14ae
bigPoppa-x <ante.petkovic01@gmail.com>
2020-11-18 12:11:08 +0100

    migrations

--- models/todo.js
+++ models/todo.js
@@ -1,18 +1,29 @@
 module.exports = (sequelize, DataTypes) => {
-    const Todo = sequelize.define('Todo', {
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        completed: {
-            type: DataTypes.BOOLEAN,
-            allowNull: false
-        },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: true
-        }        
-    }, {});
-    return Todo;
-};
+  const Todo = sequelize.define('Todo', {
+      title: {
+          allowNull: false,
+          type: DataTypes.STRING
+      },
+      completed: {
+          allowNull: false,
+          type: DataTypes.BOOLEAN
+      },
+      description: {
+          allowNull: true,
+          type: DataTypes.STRING
+      },
+      userId: {
+        allowNull: false,
+        type: DataTypes.INTEGER
+      }       
+  }, {});
 
+  Todo.associate = (models) => {
+      Todo.belongsTo(models.User, {
+        as: 'user',
+        foreignKey: 'userId'
+      });
+  };
+
+  return Todo;
+};
